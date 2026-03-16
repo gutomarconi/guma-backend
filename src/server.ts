08bd108cb386c185ca97fe9b8bf9fca41ed14d25
Gustavo Marconi <gustavo.marconi@twavectr.com>
2026-03-16 17:41:46 -0300

    removing swagger

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import machineRoutes from "./routes/machine.routes";
 import machineFingerprintRoutes from "./routes/machineFingerprint.routes";
 import authMiddleware from "./middleware/auth.middleware";
 import { tenantMiddleware } from "./middleware/tenant.middleware";
-import swaggerSpec from './swagger';
+
 import swaggerUi from 'swagger-ui-express';
 import itemRoutes from './routes/item.routes';
 import itemHistoryRoutes from './routes/itemHistory.routes';
@@ -29,7 +29,7 @@ app.use(cors({
 app.use(express.json());
 
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/apikeys", apikeyRoutes);
 
 app.use("/auth", authRoutes);
